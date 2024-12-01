ade5ae5b1ea9c213cfc7f75be35ee5c45177e4c6
MrM3ROF <117533277+MrM3ROF@users.noreply.github.com>
2024-12-01 22:03:05 +0300

    ErrorHandelr Done

--- src/main/java/org/example/common/ErrorHandler.java
+++ src/main/java/org/example/common/ErrorHandler.java
@@ -1,5 +1,12 @@
 package org.example.common;
+import javax.swing.JOptionPane;
+
 
 public class ErrorHandler {
-
+    public static void showError(String msg){
+        JOptionPane.showMessageDialog(null, msg,"Error",JOptionPane.ERROR_MESSAGE);
+    }
+    public static void handleException(Exception ex,String ermsg){
+        showError(ermsg);
+    }
 }
