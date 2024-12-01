0e272f2da4ace6ee00538c269b75b118803985fe
Muhannad Majed <mohanad.alfawzan1@gmail.com>
2024-12-01 21:51:11 +0300

    Added DBHandelr and ErrorHandler

--- src/main/java/org/example/common/DatabaseHandler.java
+++ src/main/java/org/example/common/DatabaseHandler.java
@@ -1,1 +1,17 @@
+package org.example.common;
+import java.sql.*;
 
+public class DatabaseHandler {
+    private  String DB_URL;
+    private  String DB_USERNAME;
+    private  String DB_PASSWORD;
+
+    private Connection connection;
+
+    public DatabaseHandler() {
+        try {
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+
+        }
+    }
+}
