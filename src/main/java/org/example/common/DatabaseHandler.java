1f903718a58f7a1e2d3457b93c75752f86ea0791
Muhannad Majed <mohanad.alfawzan1@gmail.com>
2024-12-01 22:12:06 +0300

    Done DBHandelr

--- src/main/java/org/example/common/DatabaseHandler.java
+++ src/main/java/org/example/common/DatabaseHandler.java
@@ -12,6 +12,46 @@ public class DatabaseHandler {
         try {
             connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
 
+        } catch (SQLException e) {
+            ErrorHandler.handleException(e, "Failed to connect to the database.");
+            throw new RuntimeException("Failed to connect to the database.");
+        }
+    }
+    public ResultSet executeQuery(String query, Object... params) throws SQLException {
+        try {
+            PreparedStatement ps = connection.prepareStatement(query);
+            setParameters(ps, params);
+            return ps.executeQuery();
+        } catch (SQLException e) {
+            ErrorHandler.handleException(e, "Failed to execute the query, Please try again.");
+            return null;
+        }
+    }
+
+    public int executeUpdate(String query, Object... params) throws SQLException {
+        try {
+            PreparedStatement ps = connection.prepareStatement(query);
+            setParameters(ps, params);
+            return ps.executeUpdate();
+        } catch (SQLException e) {
+            ErrorHandler.handleExcepetion(e, "Failed to update the database, Please try again.");
+            return -1;
+        }
+    }
+
+    private void setParameters(PreparedStatement ps, Object... params) throws SQLException {
+        for (int i = 0; i < params.length; i++) {
+            ps.setObject(i + 1, params[i]);
+        }
+    }
+    public void closeConnection() {
+        try {
+            if(connection != null) !connection.isClosed() {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            ErrorHandler.handleException(e, "Failed to close the database connection.");
+            throw new RuntimeException("Failed to close the database connection.");
         }
     }
 }
