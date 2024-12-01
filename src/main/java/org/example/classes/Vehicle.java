97f9cb22be713cce8abb206d062c0e4f679a82a6
MrM3ROF <117533277+MrM3ROF@users.noreply.github.com>
2024-12-01 21:00:39 +0300

    invoice and vehicle classes are done

--- src/main/java/org/example/classes/Vehicle.java
+++ src/main/java/org/example/classes/Vehicle.java
@@ -5,8 +5,104 @@ public class Vehicle {
     private String serialNumber;
     private String  name,color,plate,shifter,kpl;
     private double price;
-    private boolean isBooked;
 
 
+    public Vehicle(int id, int milage, int size, int modelYear, String serialNumber, String name, String color, String plate, String shifter, String kpl, double price) {
+        this.id = id;
+        this.milage = milage;
+        this.size = size;
+        this.modelYear = modelYear;
+        this.serialNumber = serialNumber;
+        this.name = name;
+        this.color = color;
+        this.plate = plate;
+        this.shifter = shifter;
+        this.kpl = kpl;
+        this.price = price;
 
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getKpl() {
+        return kpl;
+    }
+
+    public void setKpl(String kpl) {
+        this.kpl = kpl;
+    }
+
+    public String getShifter() {
+        return shifter;
+    }
+
+    public void setShifter(String shifter) {
+        this.shifter = shifter;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public int getModelYear() {
+        return modelYear;
+    }
+
+    public void setModelYear(int modelYear) {
+        this.modelYear = modelYear;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getMilage() {
+        return milage;
+    }
+
+    public void setMilage(int milage) {
+        this.milage = milage;
+    }
 }
