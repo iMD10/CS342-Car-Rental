c9f8d0b1b1efae5828694b1a3cb69a0d7fc21215
Amer_ <nnnnn>
2024-12-01 20:40:26 +0300

    BrowseVehicles added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,8 @@ import org.example.views.BrowseVehicles;
 
 public class Main {
     public static void main(String[] args) {
-        BrowseVehicles bv = new BrowseVehicles();
-        System.out.println("Abo Mohammed Car Rental Agency");
+//        BrowseVehicles bv = new BrowseVehicles();
+//        System.out.println("Abo Mohammed Car Rental Agency");
 
 
     }
