6e22caf93cf0ca61cf226083455d6789b99cf362
fsx77 <fsx770@gmail.com>
2024-12-03 00:13:55 +0300

    Joptionpane

--- src/main/java/org/example/view/BrowseVehicles.java
+++ src/main/java/org/example/view/BrowseVehicles.java
@@ -5,7 +5,7 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 
 public class BrowseVehicles extends JFrame {
-
+    private String carsCategory[]= {"Camry", "Accord", "Sonata","Yaris","Accent","Azera"};
     private final CustomerDashboard dashboard;
 
     public BrowseVehicles(CustomerDashboard dashboard) {
@@ -19,7 +19,10 @@ public class BrowseVehicles extends JFrame {
         // Top panel for date selection
         JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
         JLabel nameLabel = new JLabel("Name:");
-        JTextField nameField = new JTextField("Accord", 10);
+
+//        JTextField nameField = new JTextField("Accord", 10);
+        JComboBox nameField = new JComboBox(carsCategory);
+
         JLabel fromLabel = new JLabel("From:");
         JTextField fromDateField = new JTextField("01/01/2024", 10);
         JLabel toLabel = new JLabel("To:");
@@ -62,12 +65,23 @@ public class BrowseVehicles extends JFrame {
         // Bottom panel for buttons
         JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
         JButton backButton = new JButton("Back to Dashboard");
+        JButton bookButton = new JButton("Book Now");
+        bookButton.addActionListener(e -> {
+
+            int response = JOptionPane.showConfirmDialog(null, "Do you want to proceed?", "Confirmation", JOptionPane.YES_NO_OPTION);
+            if (response == JOptionPane.YES_OPTION) {
+                this.setVisible(false);
+                Invoices pells = new Invoices(dashboard);
+            }
+
+        });
         backButton.addActionListener(e -> {
             this.setVisible(false);
             dashboard.setVisible(true);
         });
-        bottomPanel.add(backButton);
 
+        bottomPanel.add(backButton);
+        bottomPanel.add(bookButton);
         // Add panels to the frame
         add(topPanel, BorderLayout.NORTH);
         add(tableScrollPane, BorderLayout.CENTER);
@@ -75,4 +89,6 @@ public class BrowseVehicles extends JFrame {
 
         setVisible(true);
     }
+
+
 }
