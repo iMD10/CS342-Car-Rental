46b93c7f746a418f1c5e57c39c74acd4f97cfff6
Muhannad Majed <mohanad.alfawzan1@gmail.com>
2024-12-03 18:09:52 +0300

    Adding Validation signature

---
+++ src/main/java/org/example/common/Validation.java
@@ -0,0 +1,16 @@
+package org.example.common;
+
+public class Validation {
+    public boolean checkEmail(String email) {
+
+    }
+    public boolean checkPassword(String password) {
+
+    }
+    public boolean checkName(String name) {
+
+    }
+    public boolean checkPhone(String phone){
+
+    }
+}
