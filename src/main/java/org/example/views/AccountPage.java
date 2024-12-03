a5ff1bdf9cc1cd9f9306dbf5a7042388d125a493
Amer_ <nnnnn>
2024-12-03 16:21:10 +0300

    - AccountPage refinements

--- src/main/java/org/example/views/AccountPage.java
+++ src/main/java/org/example/views/AccountPage.java
@@ -1,22 +1,13 @@
 package org.example.views;
 
-import com.formdev.flatlaf.FlatLightLaf;
-import com.github.lgooddatepicker.components.DatePicker;
-import com.github.lgooddatepicker.components.DatePickerSettings;
-import com.github.lgooddatepicker.optionalusertools.DateChangeListener;
-import com.github.lgooddatepicker.optionalusertools.DateVetoPolicy;
-import com.github.lgooddatepicker.zinternaltools.DateChangeEvent;
-
 import javax.swing.*;
-import javax.swing.text.SimpleAttributeSet;
-import javax.swing.text.StyleConstants;
 import java.awt.*;
-import java.time.LocalDate;
 
 public class AccountPage extends JFrame {
 
     private JLabel titleLabel, fNameLabel, lNameLabel, emailLabel, phoneLabel;
-    private JTextField titleTf, flNameTf, lNameTf, emailTf, phoneTf;
+    private JLabel saveChangesLabel;
+    private JTextField titleTf, fNameTf, lNameTf, emailTf, phoneTf;
     private JPanel mainPanel;
 
     AccountPage() {
@@ -29,22 +20,104 @@ public class AccountPage extends JFrame {
 
         mainPanel = new JPanel(new BorderLayout());
         JPanel contentPanel = new JPanel();
-        contentPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
         titleLabel = new JLabel("Account Page");
+        titleLabel.setFont(new Font("SansSerif", Font.BOLD, 25));
+
         fNameLabel = new JLabel("First Name");
+        fNameLabel.setFont(new Font("SansSerif", Font.PLAIN, 15));
+        fNameLabel.setPreferredSize(new Dimension(300,30));
+
         lNameLabel = new JLabel("Last Name");
+        lNameLabel.setFont(new Font("SansSerif", Font.PLAIN, 15));
+        lNameLabel.setMaximumSize(new Dimension(300, 30));
+
         emailLabel = new JLabel("Email");
+        emailLabel.setFont(new Font("SansSerif", Font.PLAIN, 15));
+        emailLabel.setMaximumSize(new Dimension(300, 30));
+
+        phoneLabel = new JLabel("Phone Number");
+        phoneLabel.setFont(new Font("SansSerif", Font.PLAIN, 15));
+        phoneLabel.setMaximumSize(new Dimension(300, 30));
+
+        saveChangesLabel = new JLabel("<html><u>Save Changes</u></html>");
+        saveChangesLabel.setFont(new Font("SansSerif", Font.PLAIN, 15));
+        saveChangesLabel.setForeground(Color.GRAY);
+        saveChangesLabel.setMaximumSize(new Dimension(300, 30));
+
+
+
+
+        fNameTf = new JTextField("Your fName", 20);
+        fNameTf.setFont(new Font("SansSerif", Font.PLAIN, 15));
+        fNameTf.setMaximumSize(new Dimension(200, 30));
+
+        lNameTf = new JTextField("Your lName", 20);
+        lNameTf.setFont(new Font("SansSerif", Font.PLAIN, 15));
+        lNameTf.setMaximumSize(new Dimension(200, 30));
+
+        emailTf = new JTextField("Your Email", 20);
+        emailTf.setFont(new Font("SansSerif", Font.PLAIN, 15));
+        emailTf.setMaximumSize(new Dimension(200, 30));
+
+        phoneTf = new JTextField("Your Phone", 20);
+        phoneTf.setFont(new Font("SansSerif", Font.PLAIN, 15));
+        phoneTf.setMaximumSize(new Dimension(200, 30));
+
+
+        contentPanel.add(makeItFlowPanel(titleLabel));
+        contentPanel.add(Box.createRigidArea(new Dimension(300, 25)));
+        contentPanel.add(Box.createVerticalGlue());
+
+        contentPanel.add(makeItFlowPanel(fNameLabel));
+        contentPanel.add((makeItFlowPanel(fNameTf)));
+        contentPanel.add(Box.createVerticalGlue());
+        contentPanel.add(makeItFlowPanel(lNameLabel));
+        contentPanel.add(makeItFlowPanel(lNameTf));
+        contentPanel.add(Box.createVerticalGlue());
+        contentPanel.add(Box.createRigidArea(new Dimension(1, 20)));
+
+
+        contentPanel.add(makeItFlowPanel(emailLabel));
+        contentPanel.add(makeItFlowPanel(emailTf));
+        contentPanel.add(Box.createVerticalGlue());
+        contentPanel.add(Box.createVerticalGlue());
+
+        contentPanel.add(makeItFlowPanel(phoneLabel));
+        contentPanel.add(makeItFlowPanel(phoneTf));
+        contentPanel.add(makeItFlowPanel(saveChangesLabel));
+        contentPanel.add(Box.createVerticalGlue());
 
 
         mainPanel.add(new Components().createSideBarPanel(), BorderLayout.WEST);
 
-        mainPanel.add(contentPanel, BorderLayout.CENTER);
+        JPanel paddingPanel = new JPanel();
+        paddingPanel.setLayout(new BoxLayout(paddingPanel, BoxLayout.X_AXIS));
+        paddingPanel.add(Box.createRigidArea(new Dimension(10,1)));
+        paddingPanel.add(contentPanel);
+        paddingPanel.add(Box.createRigidArea(new Dimension(10,1)));
+
+        mainPanel.add(paddingPanel, BorderLayout.CENTER);
 
 
         this.add(mainPanel);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
 
+    }
+
+    private JPanel makeItFlowPanel(Component label) {
+        JPanel labelPanel = new JPanel();
+        labelPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+//        labelPanel.add(Box.createRigidArea(new Dimension(100,2)));
+        labelPanel.add(label);
+//        labelPanel.add(Box.createHorizontalGlue());
+//        labelPanel.add(Box.createHorizontalGlue());
+//        labelPanel.add(Box.createHorizontalGlue());
+//        labelPanel.setBorder(BorderFactory.createLineBorder(Color.RED, 2));
+
+        return labelPanel;
     }
 
     public static void main(String[] args) {
