35001a2144b983f6ae59d2e4b68d364f26c15dd3
Muhannad Majed <mohanad.alfawzan1@gmail.com>
2024-12-04 03:50:39 +0300

    Able to connect to db

--- src/main/java/org/example/common/DatabaseHandler.java
+++ src/main/java/org/example/common/DatabaseHandler.java
@@ -3,9 +3,9 @@ package org.example.common;
 import java.sql.*;
 
 public class DatabaseHandler {
-    private static final String DB_URL = "" ;
-    private static final String DB_USERNAME = "";
-    private static final String DB_PASSWORD = "";
+    private static final String DB_URL = System.getenv("DB_URL");
+    private static final String DB_USERNAME = System.getenv("DB_USERNAME");
+    private static final String DB_PASSWORD = System.getenv("DB_PASSWORD");
 
     private Connection connection;
 
